c46a80aa2c6d79f8a5562f480cf46217fb888d3b
Pooya Behravesh <pooyabehravesh@gmail.com>
2025-10-03 23:58:03 +0330

    Updated Config EM

--- app/Models/Config.php
+++ app/Models/Config.php
@@ -2,9 +2,25 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Config extends Model
 {
-    //
+    use HasFactory;
+
+    public function station()
+    {
+        return $this->belongsTo(Station::class);
+    }
+
+    public function sensor()
+    {
+        return $this->belongsTo(Sensor::class);
+    }
+
+    public function datalogger()
+    {
+        return $this->belongsTo(DataLogger::class);
+    }
 }
--- database/migrations/2025_10_03_202239_create_configs_table.php
+++ database/migrations/2025_10_03_202239_create_configs_table.php
@@ -6,20 +6,44 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('configs', function (Blueprint $table) {
             $table->id();
+
+            $table->foreignId('station_id')->constrained()->onDelete('cascade');
+            $table->foreignId('sensor_id')->constrained()->onDelete('cascade');
+            $table->foreignId('datalogger_id')->constrained('data_loggers')->onDelete('cascade');
+
+            $table->string('time_zone')->nullable();
+            $table->integer('sample_rate')->nullable(); // دقیقه
+
+            $table->enum('connection_type', [
+                'FTP',
+                'HTTP',
+                'SMS',
+                'OFFLINE_SD'
+            ])->default('FTP');
+
+            $table->string('server_folder_name')->nullable();
+
+            $table->float('min_val')->nullable();
+            $table->float('max_val')->nullable();
+            $table->float('min_val_height')->nullable();
+            $table->float('max_val_height')->nullable();
+
+            $table->enum('sensor_data_transfer_type', ['V', 'A'])->nullable(); // ولتی یا جریانی
+
+            $table->float('offset')->nullable(); // میلی‌متر
+            $table->float('gain')->nullable(); // ضریب
+
+            $table->string('simcard_provider')->nullable();
+            $table->string('phone_number')->nullable();
+
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('configs');
