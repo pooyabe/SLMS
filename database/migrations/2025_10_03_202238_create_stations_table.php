421729e63b5a285315236d74136ffeb061dd9e68
Pooya Behravesh <pooyabehravesh@gmail.com>
2025-10-03 23:57:20 +0330

    Stations EM created

--- database/migrations/2025_10_03_202238_create_stations_table.php
+++ database/migrations/2025_10_03_202238_create_stations_table.php
@@ -6,20 +6,22 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('stations', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('state')->nullable(); // استان
+            $table->string('code')->unique(); // کد اختصاری یکتا
+            $table->string('main_contact')->nullable(); // مسئول اصلی ایستگاه
+            $table->decimal('latitude', 10, 6)->nullable(); // مختصات
+            $table->decimal('longitude', 10, 6)->nullable();
+            $table->string('reference_datum')->nullable(); // مبنای ارتفاعی
+            $table->boolean('is_online')->default(true); // آنلاین یا غیرفعال
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('stations');
