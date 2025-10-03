f787f6370d94d18aab7542249e99962ae524e3e2
Pooya Behravesh <pooyabehravesh@gmail.com>
2025-10-04 00:01:03 +0330

    User permissions updated EM

--- database/migrations/2025_10_03_202239_create_user_permissions_table.php
+++ database/migrations/2025_10_03_202239_create_user_permissions_table.php
@@ -6,20 +6,18 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('user_permissions', function (Blueprint $table) {
             $table->id();
+
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('ability'); // مثلا ورود به NRP، دانلود دیتا، مدیریت کاربران و...
+
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('user_permissions');
