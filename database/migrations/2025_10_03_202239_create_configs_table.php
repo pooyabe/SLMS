e4697fbf2b88b3ac15fdd13a578eca056489581c
Pooya Behravesh <pooyabehravesh@gmail.com>
2025-10-04 18:25:38 +0330

    Deleted some rows from Configs schema

--- database/migrations/2025_10_03_202239_create_configs_table.php
+++ database/migrations/2025_10_03_202239_create_configs_table.php
@@ -12,7 +12,6 @@ return new class extends Migration
             $table->id();
 
             $table->foreignId('station_id')->constrained()->onDelete('cascade');
-            $table->foreignId('sensor_id')->constrained()->onDelete('cascade');
             $table->foreignId('datalogger_id')->constrained('data_loggers')->onDelete('cascade');
 
             $table->string('time_zone')->nullable();
@@ -26,16 +25,6 @@ return new class extends Migration
             ])->default('FTP');
 
             $table->string('server_folder_name')->nullable();
-
-            $table->float('min_val')->nullable();
-            $table->float('max_val')->nullable();
-            $table->float('min_val_height')->nullable();
-            $table->float('max_val_height')->nullable();
-
-            $table->enum('sensor_data_transfer_type', ['V', 'A'])->nullable(); // ولتی یا جریانی
-
-            $table->float('offset')->nullable(); // میلی‌متر
-            $table->float('gain')->nullable(); // ضریب
 
             $table->string('simcard_provider')->nullable();
             $table->string('phone_number')->nullable();
