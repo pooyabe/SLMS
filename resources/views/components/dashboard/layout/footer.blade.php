f7c36900b21eb4f554ac66d39d4a509d2c6d96cb
Pooya Behravesh <pooyabehravesh@gmail.com>
2025-10-07 15:10:45 +0330

    Cutted the dashboard to sections

---
+++ resources/views/components/dashboard/layout/footer.blade.php
@@ -0,0 +1,58 @@
+
+<!-- /.content-wrapper -->
+<footer class="main-footer">
+    <strong>CopyLeft &copy; 2018 <a href="http://github.com/hesammousavi/">حسام موسوی</a>.</strong>
+</footer>
+
+<!-- Control Sidebar -->
+<aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+</aside>
+<!-- /.control-sidebar -->
+</div>
+
+
+
+
+<!-- ./wrapper -->
+
+<!-- jQuery -->
+<script src="assets/dashboard/plugins/jquery/jquery.min.js"></script>
+<!-- jQuery UI 1.11.4 -->
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
+<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+<script>
+    $.widget.bridge('uibutton', $.ui.button)
+</script>
+<!-- Bootstrap 4 -->
+<script src="assets/dashboard/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- Morris.js charts -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
+<script src="assets/dashboard/plugins/morris/morris.min.js"></script>
+<!-- Sparkline -->
+<script src="assets/dashboard/plugins/sparkline/jquery.sparkline.min.js"></script>
+<!-- jvectormap -->
+<script src="assets/dashboard/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js"></script>
+<script src="assets/dashboard/plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
+<!-- jQuery Knob Chart -->
+<script src="assets/dashboard/plugins/knob/jquery.knob.js"></script>
+<!-- daterangepicker -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.10.2/moment.min.js"></script>
+<script src="assets/dashboard/plugins/daterangepicker/daterangepicker.js"></script>
+<!-- datepicker -->
+<script src="assets/dashboard/plugins/datepicker/bootstrap-datepicker.js"></script>
+<!-- Bootstrap WYSIHTML5 -->
+<script src="assets/dashboard/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js"></script>
+<!-- Slimscroll -->
+<script src="assets/dashboard/plugins/slimScroll/jquery.slimscroll.min.js"></script>
+<!-- FastClick -->
+<script src="assets/dashboard/plugins/fastclick/fastclick.js"></script>
+<!-- AdminLTE App -->
+<script src="assets/dashboard/dist/js/adminlte.js"></script>
+<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+<script src="assets/dashboard/dist/js/pages/dashboard.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="assets/dashboard/dist/js/demo.js"></script>
+</body>
+
+</html>
