1823919aad1671fb2518a5a021aea85c1c1f175b
Pooya Behravesh <pooyabehravesh@gmail.com>
2025-10-07 14:44:55 +0330

    UI Bug Fixed

--- resources/views/dashboard/layout.blade.php
+++ resources/views/dashboard/layout.blade.php
@@ -7,7 +7,7 @@
   <!-- Tell the browser to be responsive to screen width -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="assets/dashboardss/plugins/font-awesome/css/font-awesome.min.css">
+  <link rel="stylesheet" href="assets/dashboard/plugins/font-awesome/css/font-awesome.min.css">
   <!-- Ionicons -->
   <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
   <!-- Theme style -->
