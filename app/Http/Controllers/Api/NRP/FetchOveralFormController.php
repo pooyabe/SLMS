d0efce4627b5c83f037d1dce7b28988ebc6ee260
Pooya Behravesh <pooyabehravesh@gmail.com>
2025-10-08 14:55:00 +0330

    Added fetch states tiwth nrp

---
+++ app/Http/Controllers/Api/NRP/FetchOveralFormController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers\Api\NRP;
+
+use App\Http\Controllers\Controller;
+use App\Models\Station;
+use Illuminate\Http\Request;
+
+class FetchOveralFormController extends Controller
+{
+    // Send States with defined stations
+    public function FetchStates(Request $request)
+    {
+        $user = $request->user(); // کاربری که توکنش معتبره
+
+        // اگر توکن معتبر نباشه
+        if (!$user) {
+            return response()->json([
+                'message' => '103'
+            ], 403);
+        }
+
+        try {
+            // فقط استان‌هایی که در جدول Station وجود دارن
+            $states = Station::select(['state', 'state_fa'])
+                ->whereNotNull('state')
+                ->distinct()
+                ->orderBy('state', 'asc')
+                ->get();
+
+            $formatted = $states->map(fn($s) => [
+                'label' => $s->state_fa,
+                'value' => $s->state,
+            ])->values();
+
+
+
+            return response()->json([
+                'success' => true,
+                'states' => $formatted,
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                'success' => false,
+                'error' => $e->getMessage(),
+            ], 500);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,21 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\NRP\FetchOveralFormController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('auth')->group(function () {
     Route::post('/request-code', [AuthController::class, 'requestCode']);
     Route::post('/verify-code', [AuthController::class, 'verifyCode']);
+});
+
+Route::group([
+    'as' => 'nrp.',
+    'prefix' => 'nrp',
+    'middleware' => 'auth:sanctum'
+], function () {
+
+    // Fetch states list
+    Route::post('/fetch-states', [FetchOveralFormController::class, 'FetchStates']);
 });
