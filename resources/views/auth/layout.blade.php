4ea5a43fa0bf562234f01850f0a034c2cff91383
Pooya Behravesh <pooyabehravesh@gmail.com>
2025-10-25 19:14:46 +0330

    Published the login page UI

--- resources/views/auth/layout.blade.php
+++ resources/views/auth/layout.blade.php
@@ -1,16 +1,25 @@
 <!DOCTYPE html>
 <html lang="fa">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>ورود به پنل کاربری</title>
-
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="{{ asset('assets/dashboard/plugins/font-awesome/css/font-awesome.min.css') }}">
+    <!-- template -->
     <link rel="stylesheet" href="{{ asset('assets/css/loginpage.css') }}">
+
 </head>
+
 <body>
 
     @yield('form')
-    
+
+    <script src="{{ asset('assets/dashboard/plugins/jquery/jquery.min.js') }}"></script>
 </body>
+
 </html>
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,13 +1,18 @@
 @extends('auth.layout')
 
 @section('form')
-    <form action="{{ route('auth.getphone') }}" method="post">
+    <div class="overlay"></div>
+    <form action="{{ route('auth.getphone') }}" method="post" class="form-group">
         @csrf
-        <input type="tel" name="phone" placeholder="شماره تلفن" required />
-        <button type="submit">دریافت کد</button>
+        <img src="{{ asset('assets/imgs/ncc-logo-white.png') }}" alt="Logo" class="ncc-logo" />
+        <h1 class="title">ورود به حساب کاربری</h1>
+        <input type="tel" name="phone" placeholder="شماره تلفن" required class="input_data" />
+        <button type="submit" class="submit_button">دریافت کد</button>
 
         @error('phone')
-            کاربر یافت نشد
+            <span class="error">
+                کاربر یافت نشد
+            </span>
         @enderror
     </form>
 @endsection
